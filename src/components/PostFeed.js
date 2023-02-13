922e7716b19b17ab5e176054273da5a4874ec470
Eduardo-JP-Ferreira <eduardopires2014@gmail.com>
2023-02-13 02:11:11 -0300

    Implemento da Função de Curtida

--- src/components/PostFeed.js
+++ src/components/PostFeed.js
@@ -1,54 +1,81 @@
-
 import { useState } from "react"
-export default function PostFeed(props){
-    const [salvar, setSalvar] = useState("bookmark-outline")
 
-    function salvar1(){
-        if (salvar==="bookmark-outline"){
-            setSalvar( "bookmark")
+export default function PostFeed(props) {
+    const [salvar, setSalvar] = useState("bookmark-outline")
+    const [curtir, setCurtir] = useState("heart-outline")
+    const [curtida, setCurtida] = useState(props.curtidas)
+    const [corIcone, setCorIcone] = useState("preto")
+    function botaoSalvar() {
+        if (salvar === "bookmark-outline") {
+            setSalvar("bookmark")
+
         }
-        else{
+        else {
             setSalvar("bookmark-outline")
         }
     }
-    return(
-        
+
+    function botaoCurtir() {
+        if (curtir === "heart-outline") {
+            setCurtir("heart")
+            setCorIcone("vermelho")
+            let num =Number(curtida)+1
+            setCurtida(num)
+        }
+        else {
+            setCurtir("heart-outline")
+            setCorIcone("preto")
+            let num =Number(curtida)-1
+            setCurtida(num)
+        }
+    }
+
+    function imgCurtir() {
+        if (curtir === "heart-outline") {
+            setCurtir("heart")
+            setCorIcone("vermelho")
+            let num =Number(curtida)+1
+            setCurtida(num)
+        }
+    }
+    return (
+
         <div data-test="post" class="post">
-            
-                    <div class="topo">
-                        <div class="usuario">
-                            <img src={props.imgTopo} alt={props.nomeTopo}/>
-                            {props.nomeTopo}
-                        </div>
-                        <div class="acoes">
-                            <ion-icon name="ellipsis-horizontal"></ion-icon>
-                        </div>
+
+            <div class="topo">
+                <div class="usuario">
+                    <img src={props.imgTopo} alt={props.nomeTopo} />
+                    {props.nomeTopo}
+                </div>
+                <div class="acoes">
+                    <ion-icon name="ellipsis-horizontal"></ion-icon>
+                </div>
+            </div>
+
+            <div class="conteudo">
+                <img data-test="post-image" onDoubleClick={() => imgCurtir()} src={props.imgConteudo} alt={props.nomeConteudo} />
+            </div>
+
+            <div class="fundo">
+                <div class="acoes">
+                    <div>
+                        <ion-icon data-test="like-post" onClick={() => botaoCurtir()} class={corIcone} name={curtir}></ion-icon>
+                        <ion-icon name="chatbubble-outline"></ion-icon>
+                        <ion-icon name="paper-plane-outline"></ion-icon>
                     </div>
-
-                    <div class="conteudo">
-                        <img data-test="post-image" src={props.imgConteudo} alt={props.nomeConteudo}/>
-                    </div>
-
-                    <div class="fundo">
-                        <div class="acoes">
-                            <div>
-                                <ion-icon name="heart-outline"></ion-icon>
-                                <ion-icon name="chatbubble-outline"></ion-icon>
-                                <ion-icon name="paper-plane-outline"></ion-icon>
-                            </div>
-                            <div>
-                                <ion-icon data-test="save-post" onClick={() => salvar1()} name={salvar}></ion-icon>
-                            </div>
-                        </div>
-
-                        <div class="curtidas">
-                            <img src={props.imgCurtida} alt={props.nomeCurtida}/>
-                            <div class="texto">
-                                Curtido por <strong>{props.nomeCurtida}</strong> e <strong>outras 99.159 pessoas</strong>
-                            </div>
-                        </div>
+                    <div>
+                        <ion-icon data-test="save-post" onClick={() => botaoSalvar()} name={salvar}></ion-icon>
                     </div>
                 </div>
+
+                <div class="curtidas">
+                    <img src={props.imgCurtida} alt={props.nomeCurtida} />
+                    <div class="texto">
+                        Curtido por <strong>{props.nomeCurtida}</strong> e <strong>outras <span data-test="likes-number">{curtida}</span> pessoas</strong>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -6,17 +6,22 @@ import PostFeed from "./PostFeed";
 export default function Posts(){
 
     const postagensInicial = [
-        {id: "0",imgTopo: "assets/img/meowed.svg", nomeTopo: "meowed", imgConteudo: "assets/img/gato-telefone.svg", nomeConteudo: "gato",imgCurtida:"assets/img/filomoderna.svg", nomeCurtida: "filomoderna"},
-        {id: "1",imgTopo: "assets/img/barked.svg", nomeTopo: "barked", imgConteudo: "assets/img/dog.svg", nomeConteudo: "dog",imgCurtida:"assets/img/adorable_animals.svg", nomeCurtida: "adorable_Animals"},
-        {id: "2",imgTopo: "assets/img/razoesparaacreditar.svg", nomeTopo: "razoes", imgConteudo: "assets/img/razoesparaacreditar.svg", nomeConteudo: "razao",imgCurtida: "assets/img/respondeai.svg", nomeCurtida: "respondeai"}
+        {id: "0",imgTopo: "assets/img/meowed.svg", nomeTopo: "meowed", imgConteudo: "assets/img/gato-telefone.svg", nomeConteudo: "gato",imgCurtida:"assets/img/filomoderna.svg", nomeCurtida: "filomoderna", curtidas:"241"},
+        {id: "1",imgTopo: "assets/img/barked.svg", nomeTopo: "barked", imgConteudo: "assets/img/dog.svg", nomeConteudo: "dog",imgCurtida:"assets/img/adorable_animals.svg", nomeCurtida: "adorable_Animals", curtidas:"370"},
+        {id: "2",imgTopo: "assets/img/razoesparaacreditar.svg", nomeTopo: "razoes", imgConteudo: "assets/img/razoesparaacreditar.svg", nomeConteudo: "razao",imgCurtida: "assets/img/respondeai.svg", nomeCurtida: "respondeai", curtidas:"513"}
       ]
       
     return(
         <div class="posts">
-            <PostFeed imgTopo={postagensInicial[0].imgTopo} nomeTopo={postagensInicial[0].nomeTopo} imgConteudo={postagensInicial[0].imgConteudo} nomeConteudo={postagensInicial[0].nomeConteudo} imgCurtida={postagensInicial[0].imgCurtida} nomeCurtida={postagensInicial[0].nomeCurtida}/>
+            {/* <PostFeed imgTopo={postagensInicial[0].imgTopo} nomeTopo={postagensInicial[0].nomeTopo} imgConteudo={postagensInicial[0].imgConteudo} nomeConteudo={postagensInicial[0].nomeConteudo} imgCurtida={postagensInicial[0].imgCurtida} nomeCurtida={postagensInicial[0].nomeCurtida}/>
             <PostFeed imgTopo={postagensInicial[1].imgTopo} nomeTopo={postagensInicial[1].nomeTopo} imgConteudo={postagensInicial[1].imgConteudo} nomeConteudo={postagensInicial[1].nomeConteudo} imgCurtida={postagensInicial[1].imgCurtida} nomeCurtida={postagensInicial[1].nomeCurtida}/>
-            <PostFeed imgTopo={postagensInicial[2].imgTopo} nomeTopo={postagensInicial[2].nomeTopo} imgConteudo={postagensInicial[2].imgConteudo} nomeConteudo={postagensInicial[2].nomeConteudo} imgCurtida={postagensInicial[2].imgCurtida} nomeCurtida={postagensInicial[2].nomeCurtida}/>
+            <PostFeed imgTopo={postagensInicial[2].imgTopo} nomeTopo={postagensInicial[2].nomeTopo} imgConteudo={postagensInicial[2].imgConteudo} nomeConteudo={postagensInicial[2].nomeConteudo} imgCurtida={postagensInicial[2].imgCurtida} nomeCurtida={postagensInicial[2].nomeCurtida}/> */}
 
+            {postagensInicial.map((item)=>
+                <PostFeed imgTopo={item.imgTopo} nomeTopo={item.nomeTopo} imgConteudo={item.imgConteudo} nomeConteudo={item.nomeConteudo} imgCurtida={item.imgCurtida} nomeCurtida={item.nomeCurtida} curtidas={item.curtidas}/>
+            )}
+        
+        
         </div>
     )
 }
